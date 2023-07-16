f27a0cb7d0f1526b7975afeb8cdd7d2b6cbcb9f3
Darrell-Rafa <drafa0609@gmail.com>
2023-07-16 12:47:36 +0700

    responsive home

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -12,7 +12,7 @@ function Hero() {
 
   return (
     <section id="hero">
-      <div className="h-screen bg-[url('/images/hero/bg.png')] bg-no-repeat bg-cover relative flex items-center px-24">
+      <div className="h-screen bg-[url('/images/hero/bg.png')] bg-no-repeat bg-cover relative flex items-center justify-center lg:justify-normal px-8 lg:px-24">
         <img
           src="/images/hero/telescope.png"
           alt="telescope"
@@ -22,7 +22,7 @@ function Hero() {
         />
         <div className="z-10">
           <h1
-            className={`text-[#FFD369] text-5xl mb-12 leading-relaxed ${
+            className={`text-[#FFD369] text-2xl lg:text-5xl mb-12 leading-relaxed ${
               overflow === "hidden" ? "text-left" : "text-center"
             }`}
           >
@@ -46,7 +46,7 @@ function Hero() {
               onClick={() => {
                 setOverflow("auto");
               }}
-              className="bg-[#FFC90E] border-[4px] border-black rounded-md px-8 py-2 text-2xl relative animate-scale hover:pause-animation duration-300 ease-linear"
+              className="bg-[#FFC90E] border-[4px] border-black rounded-md px-6 lg:px-8 py-2 text-lg lg:text-2xl relative animate-scale hover:pause-animation duration-300 ease-linear"
               style={{
                 appearance: "button",
                 WebkitAppearance: "button",
@@ -56,7 +56,7 @@ function Hero() {
               PLAY
             </a>
           ) : (
-            <p className="font-sans text-white text-3xl text-center font-semibold">
+            <p className="font-sans text-white text-xl lg:text-3xl text-center font-semibold">
               Let's explore your
               <br />
               childhood games with us!
--- app/components/Orbit.tsx
+++ app/components/Orbit.tsx
@@ -4,40 +4,41 @@ function Orbit() {
   return (
     <section id="games">
       <div className="h-screen bg-[url('/images/orbit/bg.png')] bg-cover bg-no-repeat relative overflow-hidden">
-        <div className="absolute top-[37.5%] left-[43.5%] w-[220px] h-[220px] border border-white animate-orbit1 rounded-full z-50">
+        <h1 className="text-white text-center my-12">Click the planet!</h1>
+        <div className="absolute top-[38%] left-[41%] lg:top-[37.5%] lg:left-[43.5%] w-[70px] h-[70px] lg:w-[220px] lg:h-[220px] border border-white animate-orbit1 rounded-full z-50">
           <a
             href="/games/80s"
-            className="absolute -top-8 w-[120px] h-[120px] bg-[url('/images/orbit/80.svg')] bg-cover bg-no-repeat flex items-center justify-center text-center text-[#55854D]"
+            className="absolute -top-8 w-[70px] h-[70px] lg:w-[120px] lg:h-[120px] bg-[url('/images/orbit/80.svg')] bg-cover bg-no-repeat flex items-center justify-center text-center text-[#55854D] text-[0.6rem] lg:text-base"
           >
             80's
             <br />
             Games
           </a>
         </div>
-        <div className="absolute top-[28.5%] left-[39%] w-[350px] h-[350px] border border-white animate-orbit2 rounded-full z-40">
+        <div className="absolute top-[31%] left-[26%] lg:top-[28.5%] lg:left-[39%] w-[180px] h-[180px] lg:w-[350px] lg:h-[350px] border border-white animate-orbit2 rounded-full z-40">
           <a
             href="/games/90s"
-            className="absolute w-[120px] h-[120px] bg-[url('/images/orbit/90.svg')] bg-cover bg-no-repeat flex items-center justify-center text-center text-[#6C4926]"
+            className="absolute -top-4 w-[70px] h-[70px] lg:w-[120px] lg:h-[120px] bg-[url('/images/orbit/90.svg')] bg-cover bg-no-repeat flex items-center justify-center text-center text-[#6C4926] text-[0.6rem] lg:text-base"
           >
             90's
             <br />
             Games
           </a>
         </div>
-        <div className="absolute top-[18%] left-[34.5%] w-[495px] h-[495px] border border-white animate-orbit3 rounded-full z-30">
+        <div className="absolute top-[25%] left-[3.25rem] lg:top-[18%] lg:left-[34.5%] w-[275px] h-[275px] lg:w-[495px] lg:h-[495px] border border-white animate-orbit3 rounded-full z-30">
           <a
             href="/games/00-09"
-            className="absolute top-8 w-[120px] h-[120px] bg-[url('/images/orbit/00.svg')] bg-cover bg-no-repeat flex items-center justify-center text-center text-[#15558C]"
+            className="absolute top-2 lg:top-8 w-[70px] h-[70px] lg:w-[120px] lg:h-[120px] bg-[url('/images/orbit/00.svg')] bg-cover bg-no-repeat flex items-center justify-center text-center text-[#15558C] text-[0.6rem] lg:text-base"
           >
             00-09
             <br />
             Games
           </a>
         </div>
-        <div className="absolute top-[9%] left-[30%] w-[625px] h-[625px] border border-white animate-orbit4 rounded-full z-20">
+        <div className="absolute top-[18%] left-0 lg:top-[9%] lg:left-[30%] w-[380px] h-[380px] lg:w-[625px] lg:h-[625px] border border-white animate-orbit4 rounded-full z-20">
           <a
             href="/games/10-16"
-            className="absolute top-14 w-[120px] h-[120px] bg-[url('/images/orbit/10.svg')] bg-cover bg-no-repeat flex items-center justify-center text-cente text-black"
+            className="absolute top-10 lg:top-14 w-[70px] h-[70px] lg:w-[120px] lg:h-[120px] bg-[url('/images/orbit/10.svg')] bg-cover bg-no-repeat flex items-center justify-center text-center text-black text-[0.6rem] lg:text-base"
           >
             10-16
             <br />
