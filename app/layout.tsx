2b6ba6bed7a1c36e086627597d316968c3aeb813
Darrell-Rafa <drafa0609@gmail.com>
2023-07-23 11:32:09 +0700

    add google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Press_Start_2P } from "next/font/google";
+import Script from "next/script";
 
 const pressStart2P = Press_Start_2P({ subsets: ["latin"], weight: ["400"] });
 
@@ -19,6 +20,18 @@ export default function RootLayout({
       <body className={`${pressStart2P.className} overflow-hidden`}>
         {children}
       </body>
+      <Script
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS_MEASUREMENT_ID}`}
+      ></Script>
+      <Script>
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.GOOGLE_ANALYTICS_MEASUREMENT_ID}');
+        `}
+      </Script>
     </html>
   );
 }
